e555635b4b16cf03bace84cf7098cc881e254f8a
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:51 +0100

    implement number parsing

--- HA1/src/complex-numbers/parser.c
+++ HA1/src/complex-numbers/parser.c
@@ -3,7 +3,12 @@
 //
 
 #include <string.h>
+#include <malloc.h>
 #include "parser.h"
+
+unsigned charIsOperation(char c) {
+    return c == '+' || c == '-';
+}
 
 int validComplexNumber(char *input) {
     unsigned hasImaginary = 0;
@@ -26,7 +31,7 @@ int validComplexNumber(char *input) {
         unsigned isDigit = input[i] >= '0' && input[i] <= '9';
         digitCount += isDigit;
 
-        unsigned isOperation = input[i] == '+' || input[i] == '-';
+        unsigned isOperation = charIsOperation(input[i]);
         operationCount += isOperation;
 
         if((operationCount > 1 && digitCount == 0) || operationCount > 2) {
@@ -55,7 +60,57 @@ int validComplexNumber(char *input) {
     return !hasSecondPart || hasImaginary;
 }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err34-c"
+void setNumberPart(char *numberString, Complex *number) {
+    size_t last = strlen(numberString) - 1;
+
+    if(numberString[last] == 'i') {
+        numberString[last] = '\0';
+        number->imaginary = atof(numberString);
+    } else {
+        number->real = atof(numberString);
+    }
+}
+#pragma clang diagnostic pop
+
 Complex parseComplexNumber(char *input) {
-    Complex result;
+    char *trimmed = malloc(strlen(input));
+
+    size_t pos = 0;
+    for(size_t i = 0; i < strlen(input); ++i) {
+        if (input[i] == ' ') {
+            continue;
+        }
+
+        trimmed[pos] = input[i];
+        ++pos;
+    }
+
+    trimmed[pos] = '\0';
+
+    Complex result = {0, 0};
+    for(size_t i = strlen(trimmed) - 1; i != 0; --i) {
+        if (!charIsOperation(trimmed[i])) {
+            continue;
+        }
+
+        setNumberPart(&trimmed[i], &result);
+
+        if(charIsOperation(trimmed[i - 1])) {
+            if(trimmed[i - 1] == '-') {
+                result.real *= -1;
+                result.imaginary *= -1;
+            }
+
+            --i;
+        }
+
+        trimmed[i] = '\0';
+    }
+
+    setNumberPart(trimmed, &result);
+
+    free(trimmed);
     return result;
 }
