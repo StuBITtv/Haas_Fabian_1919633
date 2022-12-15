947ca1a26d6a31e54a433ef99418dc8522a8cac4
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:53 +0100

    add support for single i without number prefix

--- HA1/test/cli/check-parser.c
+++ HA1/test/cli/check-parser.c
@@ -15,51 +15,111 @@ void parserTest(const char *input, double real, double imaginary) {
 
 START_TEST(only_postiv_real_part) {
     parserTest("5.0", 5, 0);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(only_negativ_real_part) {
     parserTest("-5.0", -5, 0);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(only_postiv_imaginary_part) {
     parserTest("5.0i", 0, 5);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(only_positiv_imaginary_part_single_i) {
+    parserTest("i", 0, 1);
+}
+
+END_TEST
 
 START_TEST(only_negativ_imaginary_part) {
     parserTest("-5.0i", 0, -5);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(only_negativ_imaginary_part_single_i) {
+    parserTest("-i", 0, -1);
+}
+
+END_TEST
 
 START_TEST(all_positiv_parts) {
     parserTest("5.0 + 6i", 5, 6);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(all_positiv_parts_single_i) {
+    parserTest("5.0 + i", 5, 1);
+}
+
+END_TEST
 
 START_TEST(negativ_real_part) {
     parserTest("-5.0 + 6i", -5, 6);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(negativ_imaginary_part) {
     parserTest("5.0 + -6i", 5, -6);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(negativ_imaginary_part_single_i) {
+    parserTest("5.0 + -i", 5, -1);
+}
+
+END_TEST
 
 START_TEST(subtrated_imaginary_part) {
     parserTest("5.0 - 6i", 5, -6);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(subtrated_imaginary_part_single_i) {
+    parserTest("5.0 - i", 5, -1);
+}
+
+END_TEST
 
 START_TEST(all_negativ_parts) {
     parserTest("-5.0 + -6i", -5, -6);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(negativ_real_part_subtrated_imaginary_part) {
     parserTest("-5.0 - 6i", -5, -6);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(untrimmed_parts) {
     parserTest("   -   5.0    +   6   i   ", -5, 6);
-} END_TEST
+}
+
+END_TEST
 
 START_TEST(flipped_parts) {
     parserTest("5.0i + 6", 6, 5);
-} END_TEST
+}
+
+END_TEST
+
+START_TEST(flipped_parts_single_i) {
+    parserTest("i + 6", 6, 1);
+}
+
+END_TEST
 
 Suite *parserSuite() {
     Suite *suite = suite_create("Parses complex number");
@@ -68,15 +128,21 @@ Suite *parserSuite() {
     tcase_add_test(testCase, only_postiv_real_part);
     tcase_add_test(testCase, only_negativ_real_part);
     tcase_add_test(testCase, only_postiv_imaginary_part);
+    tcase_add_test(testCase, only_positiv_imaginary_part_single_i);
     tcase_add_test(testCase, only_negativ_imaginary_part);
+    tcase_add_test(testCase, only_negativ_imaginary_part_single_i);
     tcase_add_test(testCase, all_positiv_parts);
+    tcase_add_test(testCase, all_positiv_parts_single_i);
     tcase_add_test(testCase, negativ_real_part);
     tcase_add_test(testCase, negativ_imaginary_part);
+    tcase_add_test(testCase, negativ_imaginary_part_single_i);
     tcase_add_test(testCase, subtrated_imaginary_part);
+    tcase_add_test(testCase, subtrated_imaginary_part_single_i);
     tcase_add_test(testCase, all_negativ_parts);
     tcase_add_test(testCase, negativ_real_part_subtrated_imaginary_part);
     tcase_add_test(testCase, untrimmed_parts);
     tcase_add_test(testCase, flipped_parts);
+    tcase_add_test(testCase, flipped_parts_single_i);
 
     suite_add_tcase(suite, testCase);
 
