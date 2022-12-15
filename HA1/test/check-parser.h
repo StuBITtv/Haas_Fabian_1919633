67563a21cf4682b4cee8c6e4bae3213c747a1b90
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:50 +0100

    add number parser tests

---
+++ HA1/test/check-parser.h
@@ -0,0 +1,12 @@
+//
+// Created by fhs on 14.12.2022.
+//
+
+#ifndef C_COURSE_CHECK_PARSER_H
+#define C_COURSE_CHECK_PARSER_H
+
+#include "check.h"
+
+Suite *parserSuite();
+
+#endif //C_COURSE_CHECK_PARSER_H
