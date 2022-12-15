552611956cad468114091e4ab33107ad99d28646
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:53 +0100

    add complex number in exponential format string tests

--- HA1/test/cli/check-complex-string.c
+++ HA1/test/cli/check-complex-string.c
@@ -5,12 +5,14 @@
 #include "check-complex-string.h"
 #include "check-rectangular-form-string.h"
 #include "check-polar-form-string.h"
+#include "check-exponential-form-string.h"
 
 Suite *complexNumberStringSuite() {
     Suite *suite = suite_create("Complex numbers to String");
 
     suite_add_tcase(suite, rectangularFormStringCase());
     suite_add_tcase(suite, polarFormStringCase());
+    suite_add_tcase(suite, exponentialFormStringCase());
 
     return suite;
 }
