03147748b44aaf1dc3a0d7fa47f0e57be2cd0914
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:52 +0100

    split cli functions into separate file

---
+++ HA1/src/cli.h
@@ -0,0 +1,14 @@
+//
+// Created by fhs on 15.12.2022.
+//
+
+#ifndef C_COURSE_CLI_H
+#define C_COURSE_CLI_H
+
+#include "complex-numbers/complex.h"
+
+Complex askForNumber(const char *position);
+
+char askForOperation();
+
+#endif //C_COURSE_CLI_H
--- HA1/src/main.c
+++ HA1/src/main.c
@@ -4,45 +4,15 @@
 
 #include <stdio.h>
 #include "complex-numbers/complex.h"
-
-Complex askForNumber(const char *position) {
-    char input[512];
-
-    while (1) {
-        printf("What's the %s number?\n", position);
-        scanf("%s", input);
-
-        if(validComplexNumber(input)) break;
-
-        printf("\nExpected a number in the format a+bi, instead got \"%s\".\n\n", input);
-    }
-
-    return parseComplexNumber(input);
-}
-
-char askForOperation() {
-    char input[128];
-
-    while (1) {
-        printf("What's the operation?\n");
-        scanf("%s", input);
-
-        switch (input[0]) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                return input[0];
-        }
-
-        printf("\nSupported operations are '+', '-', '*', and '/', instead got \"%s\".\n\n", input);
-    }
-}
+#include "cli.h"
 
 int main() {
     Complex first = askForNumber("first");
     printf("\n");
+
     char operation = askForOperation();
     printf("\n");
+
     Complex second = askForNumber("second");
+    printf("\n");
 }
