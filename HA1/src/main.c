8086d2c0d2ee178d0b04a430aaee0b3243ab5e45
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:51 +0100

    add number input

--- HA1/src/main.c
+++ HA1/src/main.c
@@ -2,6 +2,25 @@
 // Created by fhs on 13.12.2022.
 //
 
+#include <stdio.h>
+#include "complex-numbers/complex.h"
+
+Complex askForNumber(const char *position) {
+    char input[512];
+
+    while (1) {
+        printf("What's the %s number?\n", position);
+        scanf("%s", input);
+
+        if(validComplexNumber(input)) break;
+
+        printf("\nExpected a number in the format a+bi but got \"%s\"\n\n", input);
+    }
+
+    return parseComplexNumber(input);
+}
+
 int main() {
-    return 0;
+    Complex first = askForNumber("first");
+    Complex second = askForNumber("second");
 }
