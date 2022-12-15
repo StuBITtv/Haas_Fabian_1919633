e75354aa69b38065a92e0c9ce920e0ebba98a93f
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:52 +0100

    split cli functions into separate file

---
+++ HA1/src/cli.c
@@ -0,0 +1,40 @@
+//
+// Created by fhs on 15.12.2022.
+//
+
+#include <stdio.h>
+#include "cli.h"
+
+Complex askForNumber(const char *position) {
+    char input[512];
+
+    while (1) {
+        printf("What's the %s number?\n", position);
+        scanf("%s", input);
+
+        if(validComplexNumber(input)) break;
+
+        printf("\nExpected a number in the format a+bi, instead got \"%s\".\n\n", input);
+    }
+
+    return parseComplexNumber(input);
+}
+
+char askForOperation() {
+    char input[128];
+
+    while (1) {
+        printf("What's the operation?\n");
+        scanf("%s", input);
+
+        switch (input[0]) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return input[0];
+        }
+
+        printf("\nSupported operations are '+', '-', '*', and '/', instead got \"%s\".\n\n", input);
+    }
+}
