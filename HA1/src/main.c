4c55c71997f04471472eb952f3e1e58b4d99ea1b
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:52 +0100

    add result display

--- HA1/src/main.c
+++ HA1/src/main.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 #include "complex-numbers/complex.h"
 #include "cli.h"
 
@@ -15,4 +16,35 @@ int main() {
 
     Complex second = askForNumber("second");
     printf("\n");
+
+    Complex result;
+    switch (operation) {
+        case ADDITION:
+            result = addition(first, second);
+            break;
+        case SUBTRACTION:
+            result = subtraction(first, second);
+            break;
+        case MULTIPLICATION:
+            result = multiplication(first, second);
+            break;
+        case DIVISION:
+            result = division(first, second);
+            break;
+    }
+
+    printf("The result is\n");
+    if (result.real || !result.real && !result.imaginary) {
+        printf("%f", result.real);
+    }
+
+    if (result.real && result.imaginary) {
+        printf(result.imaginary > 0 ? " + " : " - ");
+    }
+
+    if(result.imaginary) {
+        printf("%fi", fabs(result.imaginary));
+    }
+
+    return 0;
 }
