134d3d1cbbcdccb3f03ba21aa26afade467200bc
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-15 19:52:53 +0100

    add complex number in polar format string tests

---
+++ HA1/test/cli/string-expectation.h
@@ -0,0 +1,16 @@
+//
+// Created by fhs on 15.12.2022.
+//
+
+#ifndef C_COURSE_STRING_EXPECTATION_H
+#define C_COURSE_STRING_EXPECTATION_H
+
+char *getExpectedString(
+        char firstIdentifier,
+        char secondIdentifier,
+        double firstNumber,
+        double secondNumber,
+        const char *format
+);
+
+#endif //C_COURSE_STRING_EXPECTATION_H
