c11ac14ae0556c8e4da9efbf13f5800e884e08bf
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-20 23:48:15 +0100

    setup project structure for the second exercise

---
+++ HA2/src/demo.c
@@ -0,0 +1,7 @@
+//
+// Created by fhs on 20.12.2022.
+//
+
+int main(void) {
+    return 0;
+}
---
+++ HA2/src/newton-raphson/newton-raphson.h
@@ -0,0 +1,8 @@
+//
+// Created by fhs on 20.12.2022.
+//
+
+#ifndef C_COURSE_NEWTON_RAPHSON_H
+#define C_COURSE_NEWTON_RAPHSON_H
+
+#endif //C_COURSE_NEWTON_RAPHSON_H
