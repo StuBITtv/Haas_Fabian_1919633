c120c9f911233e7811c78c0190d3456fb10ba39b
StuBITtv <29468630+StuBITtv@users.noreply.github.com>
2022-12-21 22:31:12 +0100

    add newton raphson demo

--- HA2/src/demo.c
+++ HA2/src/demo.c
@@ -1,7 +1,22 @@
 //
-// Created by fhs on 20.12.2022.
+// Created by fhs on 21.12.2022.
 //
 
-int main(void) {
-    return 0;
+#include <math.h>
+#include <function-descriptor.h>
+
+static double function(double x) {
+    return pow(x - 3, 2);
+}
+
+static double derivative(double x) {
+    return 2 * (x - 3);
+}
+
+FunctionDescriptor setFunction() {
+    return (FunctionDescriptor) {
+            "(x - 3)^2",
+            (Rational) function,
+            (Rational) derivative,
+    };
 }
